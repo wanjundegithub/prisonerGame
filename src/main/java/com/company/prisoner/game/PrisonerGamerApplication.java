d06c2c0ec331a441b7b655997c37ecea23ac606c
wanjundegithub <2304564274@qq.com>
2024-07-30 21:36:00 +0800

    first initial

---
+++ src/main/java/com/company/prisoner/game/PrisonerGamerApplication.java
@@ -0,0 +1,15 @@
+package com.company.prisoner.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author user
+ */
+@SpringBootApplication
+public class PrisonerGamerApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(PrisonerGamerApplication.class, args);
+    }
+}
